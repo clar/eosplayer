1d066d15345cc2a08a7feebd0169624915c1371c
nano <nano@gmail.com>
2019-03-04 17:53:42 +0800

    get Mykey account sign key from active perm

--- src/helpers/mykeyPlugins.js
+++ src/helpers/mykeyPlugins.js
@@ -1,5 +1,8 @@
 'use strict'
 
+import Log_ from '../utils/log'
+const log = Log_('MykeyPlugins')
+
 /**
  * Mykey plugins help to sign and validate signature
  */
@@ -13,10 +16,30 @@ export class MykeyPlugins {
     this._config = config
   }
 
-  async getSignKey(account) {
+  /**
+     * get Mykey account sign key
+     * @param {string} account - Mykey account
+     * @return {string} 
+     */
+  async getMykeyAccountSignKey(account) {
+    const { permissions } = await this._chain.getAccountInfo(account)
+    if (!permissions) {
+      log.info(`permissions of account ${account} are not found.`)
+      return
+    }
+
+    const perm = permissions.find(p => p.perm_name === "active")
+    log.info(`perm : ${JSON.stringify(perm)}`)
+    const { accounts} = perm.required_auth
+    let mgrcontract = accounts[0].permission.actor
+
+    return await this.getSignKey(account, mgrcontract)
+  }
+
+  async getSignKey(account, mgrcontract) {
     let mykey_signkey_table = "keydata" 
     let mykey_signkey_index = 3
-    let keydata = await this._chain.getTable(this._config.mgrcontract, mykey_signkey_table, account, mykey_signkey_index, mykey_signkey_index + 1);
+    let keydata = await this._chain.getTable(mgrcontract, mykey_signkey_table, account, mykey_signkey_index, mykey_signkey_index + 1);
     if(!keydata) return "";
 
     return keydata[0].key.pubkey;
@@ -26,7 +49,7 @@ export class MykeyPlugins {
     let mgr_active_perm = `${this._config.mgrcontract}@active`
     let plugin = {
       [mgr_active_perm] : async (account) => {
-        return await this.getSignKey(account)
+        return await this.getSignKey(account, this._config.mgrcontract)
       }
     }
 
@@ -37,7 +60,7 @@ export class MykeyPlugins {
     let mgr_active_perm = `${this._config.mgrcontract}@active`
     let plugin = {
       [mgr_active_perm] : async (account, recoverKey) => {
-        let pubkey = await this.getSignKey(account)
+        let pubkey = await this.getSignKey(account, this._config.mgrcontract)
         return pubkey === recoverKey
       }
     }
